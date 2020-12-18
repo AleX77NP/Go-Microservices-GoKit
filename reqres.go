1db8115c91ac40f1b81e1560638c4dada2a5aec1
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2020-12-18 22:24:22 +0100

    endpoints, req, res...

--- reqres.go
+++ reqres.go
@@ -1,2 +1,41 @@
 package todo
 
+type (
+	//AddTodoRequest ...
+	AddTodoRequest struct {
+		ID string `json:"id"`
+		Text string `json:"text"`
+		Completed bool `json:"completed"`
+	}
+	//AddTodoResponse ...
+	AddTodoResponse struct {
+		Ok string `json:"ok"`
+	}
+	// GetByIDRequest ...
+	GetByIDRequest struct {
+		ID string `json:"id"`
+	}
+	// GetByIDResponse ...
+	GetByIDResponse struct {
+		Text string `json:"text"`
+		Completed bool `json:"completed"`
+	}
+	//DeleteTodoRequest ...
+	DeleteTodoRequest struct {
+		ID string `json:"id"`
+	}
+	//DeleteTodoResponse ...
+	DeleteTodoResponse struct {
+		Deleted string `json:"deleted"`
+	}
+	//UpdateTodoRequest ...
+	UpdateTodoRequest struct {
+		ID string `json:"id"`
+		Text string `json:"text"`
+		Completed bool `json:"completed"`
+	}
+	//UpdateTodoResponse ...
+	UpdateTodoResponse struct {
+		Updated string `json:"updated"`
+	}
+)
